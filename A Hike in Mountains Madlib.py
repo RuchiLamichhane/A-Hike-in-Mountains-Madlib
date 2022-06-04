68b251b414f58b8737a1061bdd9426827e27f3ba
Ruchi Lamichhane <ruchilamichhane2468@gmail.com>
2022-06-04 11:57:18 +0545

    Create A Hike in Mountains Madlib.py

---
+++ A Hike in Mountains Madlib.py
@@ -0,0 +1,26 @@
+noun1 = input("Noun:")
+adj1 = input("Adjective:")
+food1 = input("Food:")
+food2 = input("Food:")
+partofbody = input("Part of Body:")
+noun2 = input("Noun:")
+verbing = input("Verb-ing:")
+verb1 = input("Verb:")
+color1 = input("Color:")
+color2 = input("Color:")
+adj2 = input("Adjective:")
+noun3 = input("Noun:")
+adj3 = input("Adjective:")
+animal1 = input("Animal:")
+adj4 = input("Adjective:")
+animal2 = input("Animal:")
+verb2 = input("Verb:")
+adj5 = input("Adjective:")
+verb3 = input("Verb:")
+noun4 = input("Noun:")
+madlib = f'''Our {noun1} are packed for a hike in the {adj1} Mountains!
+We are carrying a picnic lunch with {food1} and {food2} on our {partofbody}, and I have a {noun2}for {verbing} photos.
+We will {verb1} past meadows filled with {color1} and {color2} flowers,and pass a {adj2} waterfall that sounds like a {noun3}.
+Many animals live in the mountains,like the {adj3} {animal1} and {adj4} {animal2}.It's fun to {verb2} them but important to be safe and keep a {adj5} distance.
+The higher we {verb3},the smaller the wildflower meadow and waterfall appear;we may even see {noun4} still on the ground from last winter.'''
+print(madlib)
